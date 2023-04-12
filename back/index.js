acd0185309a79d1619bee4ccbf1bdd4ac7dca9dd
joaco <joacolerer@gmail.com>
2023-04-12 18:06:09 -0300

    uibuild

--- back/index.js
+++ back/index.js
@@ -1,7 +1,25 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.static('build'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(express.json())
+app.use(requestLogger)
+
 
 
 let persons = [
@@ -66,7 +84,11 @@ app.post('/api/persons', (req, res)=>{
     res.json(newEntry)
 })
 
-const PORT = 3001
+
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
