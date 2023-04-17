140d0acee5a983fcded84c665fb69c49ecb6ada8
Joaquin Lerer <joacolerer@gmail.com>
2023-04-17 19:02:05 -0300

    uibuild

--- back/models/person.js
+++ back/models/person.js
@@ -1,45 +1,50 @@
-const mongoose = require('mongoose')
+/* eslint-disable linebreak-style */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log('connecting to', url);
 
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log('connected to MongoDB');
+    console.log('nisman');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
-    id:Number,
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
+  id: Number,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator(v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
     },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: function(v) {
-                return /^\d{2,3}-\d+$/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        required: [true, 'User phone number required']
-    },
-})
+    required: [true, 'User phone number required'],
+  },
+});
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject.id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject.id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
--- back/mongo.js
+++ back/mongo.js
@@ -1,43 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const password = process.argv[2]
-const url =
-    `mongodb+srv://joacolerer:${password}@db.tjzuvny.mongodb.net/phoneBook?retryWrites=true&w=majority`
+const password = process.argv[2];
+const url = `mongodb+srv://joacolerer:${password}@db.tjzuvny.mongodb.net/phoneBook?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
 const personSchema = new mongoose.Schema({
-    id:Number,
-    name: String,
-    number: String,
-})
-const Person = mongoose.model('Person', personSchema)
+  id: Number,
+  name: String,
+  number: String,
+});
+const Person = mongoose.model('Person', personSchema);
 
-if (process.argv.length>3) {
-    const name = process.argv[3]
-    const phone = process.argv[4]
-    
-    const person = new Person({
-        id:44,
-        name: name,
-        number: phone,
-    })
+if (process.argv.length > 3) {
+  const name = process.argv[3];
+  const phone = process.argv[4];
 
-    person.save().then(result => {
-        console.log('person saved!')
-        mongoose.connection.close()
-    })
-} else{
-    Person.find({}).then(result => {
-        console.log("phonebook:")
-        result.forEach(person => {
-          console.log(`${person.name} - ${person.number}`)
-        })
-        mongoose.connection.close()
-    })
+  const person = new Person({
+    id: 44,
+    name,
+    number: phone,
+  });
+
+  person.save().then(() => {
+    console.log('person saved!');
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((result) => {
+    console.log('phonebook:');
+    result.forEach((person) => {
+      console.log(`${person.name} - ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
 }
-
-
-
-
-
